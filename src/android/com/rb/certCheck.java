e7883ea4e7bceb7fae71e46c78b1796a718ecd56
Roushan Kumar <roushankumar0102@gmail.com>
2020-04-22 13:20:54 +0530

    ewfjkew
    ewfjkbfewk

--- src/android/com/rb/certCheck.java
+++ src/android/com/rb/certCheck.java
@@ -14,10 +14,15 @@ import org.json.JSONException;
 
 import android.util.Log;
 import java.util.Date;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
 
 public class certCheck extends CordovaPlugin {
   private static final String TAG = "cdvRootBeer";
-
+  private static final String APP_SIGNATURE = "1038C0E34658923C4192E61B16846";
   public void initialize(CordovaInterface cordova, CordovaWebView webView) {
     super.initialize(cordova, webView);
     Log.d(TAG, "Initializing cdvRootBeer");    
@@ -26,8 +31,38 @@ public class certCheck extends CordovaPlugin {
 
   public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
 
-    callbackContext.success(fingerprint);
+    PackageInfo packageInfo = context.getPackageManager().getPackageInfo(
+				getPackageName(), PackageManager.GET_SIGNATURES);
+    //note sample just checks the first signature
+		for (Signature signature : packageInfo.signatures) {
+			// SHA1 the signature
+			String sha1 = getSHA1(signature.toByteArray());
+			// check is matches hardcoded value
+      callbackContext.success(APP_SIGNATURE.equals(sha1));
+		}
     return true;
   }
+
+  //computed the sha1 hash of the signature
+  public static String getSHA1(byte[] sig) {
+    MessageDigest digest = MessageDigest.getInstance("SHA1");
+    digest.update(sig);
+    byte[] hashtext = digest.digest();
+    return bytesToHex(hashtext);
+}
+
+//util method to convert byte array to hex string
+public static String bytesToHex(byte[] bytes) {
+  final char[] hexArray = { '0', '1', '2', '3', '4', '5', '6', '7', '8',
+      '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+  char[] hexChars = new char[bytes.length * 2];
+  int v;
+  for (int j = 0; j < bytes.length; j++) {
+    v = bytes[j] & 0xFF;
+    hexChars[j * 2] = hexArray[v >>> 4];
+    hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+  }
+  return new String(hexChars);
+}
 
 }
