8e8b25a7a05e9bed07504023325daeacb149a154
Roushan Kumar <roushankumar0102@gmail.com>
2020-04-22 13:11:55 +0530

    svesvgwe
    ewwgew

--- www/plugin.js
+++ www/plugin.js
@@ -15,45 +15,6 @@ var PLUGIN_NAME = 'certCheck';
 var certCheck = {
   isRooted: function (cb, err) {
     exec(cb, err, PLUGIN_NAME, 'isRooted', []);
-  },
-  isRootedWithoutBusyBoxCheck: function (cb, err) {
-    exec(cb, err, PLUGIN_NAME, 'isRootedWithoutBusyBoxCheck', []);
-  },
-  isSelinuxFlagInEnabled: function (cb, err) {
-    exec(cb, err, PLUGIN_NAME, 'isSelinuxFlagInEnabled', []);
-  },
-  detectRootManagementApps: function (cb, err) {
-    exec(cb, err, PLUGIN_NAME, 'detectRootManagementApps', []);
-  },
-  detectPotentiallyDangerousApps: function (cb, err) {
-    exec(cb, err, PLUGIN_NAME, 'detectPotentiallyDangerousApps', []);
-  },
-  detectTestKeys: function (cb, err) {
-    exec(cb, err, PLUGIN_NAME, 'detectTestKeys', []);
-  },
-  detectRootCloakingApps: function (cb, err) {
-    exec(cb, err, PLUGIN_NAME, 'detectRootCloakingApps', []);
-  },
-  checkForBusyBoxBinary: function (cb, err) {
-    exec(cb, err, PLUGIN_NAME, 'checkForBusyBoxBinary', []);
-  },
-  checkForSuBinary: function (cb, err) {
-    exec(cb, err, PLUGIN_NAME, 'checkForSuBinary', []);
-  },
-  checkSuExists: function (cb, err) {
-    exec(cb, err, PLUGIN_NAME, 'checkSuExists', []);
-  },
-  checkForRWPaths: function (cb, err) {
-    exec(cb, err, PLUGIN_NAME, 'checkForRWPaths', []);
-  },
-  checkForDangerousProps: function (cb, err) {
-    exec(cb, err, PLUGIN_NAME, 'checkForDangerousProps', []);
-  },
-  checkForRootNative: function (cb, err) {
-    exec(cb, err, PLUGIN_NAME, 'checkForRootNative', []);
-  }, 
-  checkForMagiskBinary: function (cb,err) {
-    exec(cb, err, PLUGIN_NAME, 'checkForMagiskBinary', []);
   }
 };
 
